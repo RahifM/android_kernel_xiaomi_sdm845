244c290a108fbcf5bee4a86d6fbb7f14e37f6ee3
rahifm <rahifmanjatha372@gmail.com>
2023-06-04 13:07:29 +0000

    Add commit msg along with the zip upload

--- build_master.sh
+++ build_master.sh
@@ -42,7 +42,7 @@ ramdisk_compression=auto
 	rm arter97-beryllium-$VERSION-tmp.zip
 	cd ..
 	ls -al arter97-beryllium-$VERSION.zip
-	$TG -f arter97-beryllium-$VERSION.zip "$(cat $KERNELDIR/include/generated/uts* | cut -d '"' -f 2)"$'\n'$'\n'"$(cat $KERNELDIR/include/generated/comp*h | grep LINUX_COMPILER | cut -d '"' -f 2)"
+	$TG -f arter97-beryllium-$VERSION.zip "$(cat $KERNELDIR/include/generated/uts* | cut -d '"' -f 2)"$'\n'$'\n'"$(cat $KERNELDIR/include/generated/comp*h | grep LINUX_COMPILER | cut -d '"' -f 2)"$'\n'$'\n'"HEAD: $(git log -n 1 --oneline)"
 	mv $LOG bl-$(ls arter*zip | rev | cut -d / -f 1 | rev | cut -d . -f 2 | cut -d - -f 2-).txt
 	$TG -f $LOG
 	# TEMP
