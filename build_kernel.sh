0c889cdc6308c5587b3f6348ed56eead6bdf58c1
RahifM <rahifmanjatha372@gmail.com>
2023-06-07 03:24:32 +0530

    Revert "Update build script to support /out"
    This reverts commit 0e1cc07d6c91c81c42884025cc28e39b6d26131e.

--- build_kernel.sh
+++ build_kernel.sh
@@ -98,7 +98,7 @@ cd $KERNELDIR
 
 echo "Making new boot image"
 mkbootimg \
-    --kernel $KERNELDIR/out/arch/arm64/boot/Image.gz-dtb \
+    --kernel $KERNELDIR/arch/arm64/boot/Image.gz-dtb \
     --ramdisk $RAMFS_TMP.cpio.gz \
     --cmdline 'console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 buildvariant=user printk.devkmsg=on' \
     --base           0x00000000 \
