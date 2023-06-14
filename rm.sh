b82aa64d502494e0a7630949dcf2fd9009be0ea3
RahifM <rahifmanjatha372@gmail.com>
2023-06-14 23:12:52 +0530

    Update warning rm

--- rm.sh
+++ rm.sh
@@ -1,2 +1,6 @@
 rm -rf arch/arm64/kernel/vdso32/sigreturn.o
-rm -rf arch/arm64/kernel/bootinfo.o
+rm -rf kernel/extable.o
+rm -rf kernel/printk/printk.o
+rm -rf kernel/sched/core.o
+rm -rf kernel/time/posix-cpu-timers.o
+rm -rf kernel/profile.o
