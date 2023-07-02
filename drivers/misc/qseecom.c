a1fc8960063ce0db94191d6748d3100d5fe00569
RahifM <rahifmanjatha372@gmail.com>
2023-07-03 04:33:22 +0530

    WIP: Import drivers/misc from https://github.com/RahifM/android_kernel_xiaomi_sdm845/commit/7680936256f672eff41fbe7dc2eaa3537a73127a
     Conflicts:
    	drivers/misc/Makefile
    	drivers/misc/qseecom.c

--- drivers/misc/qseecom.c
+++ drivers/misc/qseecom.c
@@ -417,6 +417,7 @@ static int get_qseecom_keymaster_status(char *str)
 }
 __setup("androidboot.keymaster=", get_qseecom_keymaster_status);
 
+<<<<<<< HEAD
 
 #define QSEECOM_SCM_EBUSY_WAIT_MS 30
 #define QSEECOM_SCM_EBUSY_MAX_RETRY 67
@@ -439,6 +440,9 @@ static int __qseecom_scm_call2_locked(uint32_t smc_id, struct scm_desc *desc)
 			(retry_count++ < QSEECOM_SCM_EBUSY_MAX_RETRY));
 	return ret;
 }
+=======
+extern void read_qseelog_wakeup(void);
+>>>>>>> 305b7e09074e (Import drivers/misc from https://github.com/RahifM/android_kernel_xiaomi_sdm845/commit/7680936256f672eff41fbe7dc2eaa3537a73127a)
 
 static int qseecom_scm_call2(uint32_t svc_id, uint32_t tz_cmd_id,
 			const void *req_buf, void *resp_buf)
@@ -1097,6 +1101,8 @@ static int qseecom_scm_call2(uint32_t svc_id, uint32_t tz_cmd_id,
 		svc_id, tz_cmd_id, qseos_cmd_id, smc_id, desc.arginfo);
 	pr_debug("scm_resp->result = 0x%x, scm_resp->resp_type = 0x%x, scm_resp->data = 0x%x\n",
 		scm_resp->result, scm_resp->resp_type, scm_resp->data);
+
+	read_qseelog_wakeup();
 	return ret;
 }
 
