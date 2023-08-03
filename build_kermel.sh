9445178c809e9e3a043cf21b3d4a8003a8464ff7
RahifM <rahifmanjatha372@gmail.com>
2023-08-03 21:28:17 +0530

    Add support for ccache

--- build_kermel.sh
+++ build_kermel.sh
@@ -4,7 +4,7 @@
 export TZ='Asia/Kolkata' && date
 export KERNELDIR=`readlink -f .`
 echo "kerneldir = $KERNELDIR"
-#export CCACHE_DIR=$PWD/../ccberyllium
+export CCACHE_DIR=$PWD/../ccberyllium
 
 TG=$HOME/telegram.sh/telegram
 TGSTKR="curl https://api.telegram.org/bot${BOT_API_KEY}/sendSticker -d "chat_id=${CHAT_ID}" -d sticker="
