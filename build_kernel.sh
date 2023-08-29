6a52cc24778a70855fb7fc4bbf8589965d035854
RahifM <rahifmanjatha372@gmail.com>
2023-08-30 02:16:32 +0530

    build: Add the eta thingy to the just build part

--- build_kernel.sh
+++ build_kernel.sh
@@ -55,7 +55,9 @@ if [ "$(grep Image.gz $LOG | cut -d / -f 4)" == "" ] ; then
 	exit 1
 fi
 
-#$TG -f $LOG "Kernel compilation success."
+#duration=$SECONDS
+#echo "Build succsssful. ($(($duration / 60)) minute(s) and $(($duration % 60)) seconds)"
+#$TG -f $LOG "Build successful. ($(($duration / 60)) minute(s) and $(($duration % 60)) seconds)"
 
 pack() {
 VERSION="g$(git rev-parse --verify --short=8 HEAD 2>/dev/null)-$(date +'%Y%m%d-%H%M')"
