bdf1678007d8d11ff58af52620a772b8903dc08f
RahifM <rahifmanjatha372@gmail.com>
2023-10-27 03:27:24 +0530

    Update build script
     * Update defconfig name
     * Use Image.gz-dtb
     * Update for /out build
     * Update for AnyKernel3
     * Fix log output
     * Add back localversion output

--- build_kermel.sh
+++ build_kermel.sh
@@ -46,7 +46,7 @@ rm -rf beryllium*.zip
 
 # Start the build
 SECONDS=0
-make beryllium_defconfig
+make beryllium_user_defconfig
 time make -j$(nproc --all) 2>&1 | tee bl-$(date +'%Y%m%d-%H%M').txt
 
 if [ "$(grep Image.gz $LOG | cut -d / -f 4)" == "" ] ; then
@@ -57,33 +57,21 @@ if [ "$(grep Image.gz $LOG | cut -d / -f 4)" == "" ] ; then
 	exit 1
 fi
 
-duration=$SECONDS
-echo "Build successful. ($(($duration / 60)) minute(s) and $(($duration % 60)) seconds)"
-$TG -f $LOG "Build successful. ($(($duration / 60)) minute(s) and $(($duration % 60)) seconds)"
-$TGSTKR"CAACAgIAAx0CZHWblQACGmtk7EsOE7CE28UD_n4bcQQXr_pKTwACPgADtEzqKETA00xcaOSEMAQ"
+#duration=$SECONDS
+#echo "Build successful. ($(($duration / 60)) minute(s) and $(($duration % 60)) seconds)"
+#$TG -f $LOG "Build successful. ($(($duration / 60)) minute(s) and $(($duration % 60)) seconds)"
+#$TGSTKR"CAACAgIAAx0CZHWblQACGmtk7EsOE7CE28UD_n4bcQQXr_pKTwACPgADtEzqKETA00xcaOSEMAQ"
 
 pack() {
-VERSION="$(cat version)-g$(git rev-parse --verify --short=8 HEAD 2>/dev/null)-$(date +'%Y%m%d-%H%M')"
+VERSION="g$(git rev-parse --verify --short=8 HEAD 2>/dev/null)-$(date +'%Y%m%d-%H%M')"
 
 rm beryllium-$VERSION.zip 2>/dev/null
 
-# Pack AnyKernel2
+# Pack AnyKernel3
 rm -rf kernelzip
-mkdir -p kernelzip/dtbs
-cp arch/arm64/boot/Image.gz kernelzip/
-find arch/arm64/boot -name '*.dtb' -exec cp {} kernelzip/dtbs/ \;
-echo "
-kernel.string=kernel $(cat version) @ xda-developers
-do.devicecheck=1
-do.modules=0
-do.cleanup=1
-do.cleanuponabort=0
-device.name1=beryllium
-block=/dev/block/bootdevice/by-name/boot
-is_slot_device=auto
-ramdisk_compression=auto
-" > kernelzip/props
-cp -rp $KERNELDIR/anykernel2/* kernelzip/
+mkdir -p kernelzip/
+cp out/arch/arm64/boot/Image.gz-dtb kernelzip/
+cp -rp $KERNELDIR/anykernel3/* kernelzip/
 cd kernelzip/
 zip -r9 beryllium-$VERSION.zip *
 cp beryllium-$VERSION.zip ../beryllium-$VERSION.zip
@@ -94,8 +82,8 @@ up() {
 # Upload package
 duration=$SECONDS
 echo "Build successful. ($(($duration / 60)) minute(s) and $(($duration % 60)) seconds)"
-$TG -f beryllium-$VERSION.zip "Build successful. ($(($duration / 60)) minute(s) and $(($duration % 60)) seconds)"$'\n'$'\n'"$(ls bery*)"$'\n'$'\n'"$(cat $KERNELDIR/include/generated/comp*h | grep LINUX_COMPILER | cut -d '"' -f 2)"$'\n'$'\n'"$(cat $KERNELDIR/include/generated/comp*h | grep UTS_VERSION | cut -d '"' -f 2)"
-mv $LOG bl-$(ls bery*zip | rev | cut -d / -f 1 | rev | cut -d . -f 2 | cut -d - -f 3-).txt
+$TG -f beryllium-$VERSION.zip "Build successful. ($(($duration / 60)) minute(s) and $(($duration % 60)) seconds)"$'\n'$'\n'"$(ls bery*)"$'\n'$'\n'"$(cat $KERNELDIR/out/include/generated/uts* | cut -d '"' -f 2)"$'\n'$'\n'"$(cat $KERNELDIR/out/include/generated/comp*h | grep LINUX_COMPILER | cut -d '"' -f 2)"$'\n'$'\n'"$(cat $KERNELDIR/out/include/generated/comp*h | grep UTS_VERSION | cut -d '"' -f 2)"
+mv $LOG bl-$(ls bery*zip | rev | cut -d / -f 1 | rev | cut -d . -f 1 | cut -d - -f 2-).txt
 $TG -f $LOG
 # Grep warnings if any
 grep warning* $LOG > w$(ls $LOG | rev | cut -d / -f 1 | rev)
@@ -107,4 +95,4 @@ fi
 $TGSTKR"CAACAgIAAx0CZHWblQACGmtk7EsOE7CE28UD_n4bcQQXr_pKTwACPgADtEzqKETA00xcaOSEMAQ"
 }
 
-#pack && up
+pack && up
