c7370038b4cfcca1faae8aebefd466653d878bb4
RahifM <rahifmanjatha372@gmail.com>
2023-11-19 01:44:39 +0530

    build: Update gcc

--- build_kermel.sh
+++ build_kermel.sh
@@ -23,12 +23,12 @@ fi
 if [ -d "arm32-gcc" ]; then
 	echo "arm32-gcc already exists"
 else
-	time git clone https://github.com/rahifm/arm32-gcc -b 9 --depth 1
+	time git clone https://github.com/rahifm/arm32-gcc -b master --depth 1
 fi
 if [ -d "arm64-gcc" ]; then
 	echo "arm64-gcc already exists"
 else
-	time git clone https://github.com/rahifm/arm64-gcc -b 9 --depth 1
+	time git clone https://github.com/rahifm/arm64-gcc -b master --depth 1
 fi
 
 if [ "$(whoami)" == "gitpod" ]; then
