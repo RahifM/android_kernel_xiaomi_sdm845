45a587b9547ddf3f8bee9c3a6bfc989e603ae711
RahifM <rahifmanjatha372@gmail.com>
2024-05-22 20:33:25 +0530

    build: zip out: Move commit id behind build date
     * beryllium-20240522-2033-g251c742c.zip

--- build_kermel.sh
+++ build_kermel.sh
@@ -63,7 +63,7 @@ fi
 #$TGSTKR"CAACAgIAAx0CZHWblQACGmtk7EsOE7CE28UD_n4bcQQXr_pKTwACPgADtEzqKETA00xcaOSEMAQ"
 
 pack() {
-VERSION="g$(git rev-parse --verify --short=8 HEAD 2>/dev/null)-$(date +'%Y%m%d-%H%M')"
+VERSION="$(date +'%Y%m%d-%H%M')-g$(git rev-parse --verify --short=8 HEAD 2>/dev/null)"
 
 rm beryllium-$VERSION.zip 2>/dev/null
 
